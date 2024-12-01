04af3a0e3d9b88ba57c6e41f542d58f44b7e23d5
Danny Jones <dpjones09@gmail.com>
2024-12-01 09:56:41 -0600

    Update day01.rb

--- 2024/day01.rb
+++ 2024/day01.rb
@@ -8,6 +8,6 @@ puts "Part 1: #{part1}"
 
 
 #### Part 2
-right_list_occurrences = right.tally
-part2 = left.sum { |l| l * right_list_occurrences[l].to_i }
+right_tally = right.tally
+part2 = left.sum { |l| l * right_tally[l].to_i }
 puts "Part 2: #{part2}"
